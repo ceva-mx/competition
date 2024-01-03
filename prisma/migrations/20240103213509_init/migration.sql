da453d2ca4a9259b5cb813e064a36d0850c62d25
ceva-gp <viacheslav.petrov@goalprofit.com>
2024-01-03 22:56:48 +0100

    add prisma

---
+++ prisma/migrations/20240103213509_init/migration.sql
@@ -0,0 +1,79 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "uuid" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("uuid")
+);
+
+-- CreateTable
+CREATE TABLE "Participant" (
+    "uuid" TEXT NOT NULL,
+    "userUuid" TEXT NOT NULL,
+    "competitionUuid" TEXT NOT NULL,
+
+    CONSTRAINT "Participant_pkey" PRIMARY KEY ("uuid")
+);
+
+-- CreateTable
+CREATE TABLE "Admin" (
+    "uuid" TEXT NOT NULL,
+    "userUuid" TEXT NOT NULL,
+    "competitionUuid" TEXT NOT NULL,
+
+    CONSTRAINT "Admin_pkey" PRIMARY KEY ("uuid")
+);
+
+-- CreateTable
+CREATE TABLE "Competition" (
+    "uuid" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "posterUrl" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+
+    CONSTRAINT "Competition_pkey" PRIMARY KEY ("uuid")
+);
+
+-- CreateTable
+CREATE TABLE "Route" (
+    "uuid" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "category" TEXT NOT NULL,
+    "cost" INTEGER NOT NULL,
+    "competitionUuid" TEXT NOT NULL,
+
+    CONSTRAINT "Route_pkey" PRIMARY KEY ("uuid")
+);
+
+-- CreateTable
+CREATE TABLE "Result" (
+    "uuid" TEXT NOT NULL,
+    "result" INTEGER NOT NULL,
+    "attempts" INTEGER NOT NULL,
+    "routeUuid" TEXT NOT NULL,
+    "participantUuid" TEXT NOT NULL,
+
+    CONSTRAINT "Result_pkey" PRIMARY KEY ("uuid")
+);
+
+-- AddForeignKey
+ALTER TABLE "Participant" ADD CONSTRAINT "Participant_userUuid_fkey" FOREIGN KEY ("userUuid") REFERENCES "User"("uuid") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Participant" ADD CONSTRAINT "Participant_competitionUuid_fkey" FOREIGN KEY ("competitionUuid") REFERENCES "Competition"("uuid") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Admin" ADD CONSTRAINT "Admin_userUuid_fkey" FOREIGN KEY ("userUuid") REFERENCES "User"("uuid") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Admin" ADD CONSTRAINT "Admin_competitionUuid_fkey" FOREIGN KEY ("competitionUuid") REFERENCES "Competition"("uuid") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Route" ADD CONSTRAINT "Route_competitionUuid_fkey" FOREIGN KEY ("competitionUuid") REFERENCES "Competition"("uuid") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Result" ADD CONSTRAINT "Result_routeUuid_fkey" FOREIGN KEY ("routeUuid") REFERENCES "Route"("uuid") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Result" ADD CONSTRAINT "Result_participantUuid_fkey" FOREIGN KEY ("participantUuid") REFERENCES "Participant"("uuid") ON DELETE RESTRICT ON UPDATE CASCADE;
---
+++ prisma/seed.ts
@@ -0,0 +1,128 @@
+import { PrismaClient } from '../node_modules/.prisma/client';
+import { faker } from '@faker-js/faker';
+
+import type {
+  User,
+  Competition,
+  Route,
+  Admin,
+  Result,
+  Participant,
+} from '../node_modules/.prisma/client';
+
+const prisma = new PrismaClient();
+
+function createUser(): User {
+  return {
+    uuid: faker.string.uuid(),
+    name: faker.person.fullName(),
+    email: faker.internet.email(),
+  };
+}
+
+function createCompetition(): Competition {
+  return {
+    uuid: faker.string.uuid(),
+    name: faker.company.name(),
+    posterUrl: faker.image.urlLoremFlickr({ category: 'nature' }),
+    description: faker.commerce.productDescription(),
+  }
+}
+
+function createParticipant(userUuid: string, competitionUuid: string): Participant {
+  return {
+    uuid: faker.string.uuid(),
+    userUuid,
+    competitionUuid,
+  };
+}
+
+const categories = ['5a', '5b', '5c', '6a', '6b', '6c', '7a', '7b', '7c', '8a', '8b', '8c'];
+function createRoute(competitionUuid: string): Route {
+  return {
+    uuid: faker.string.uuid(),
+    name: faker.commerce.product(),
+    category: categories[Math.floor(Math.random() * categories.length)],
+    cost: faker.number.int({ min: 20, max: 150 }),
+    competitionUuid,
+  };
+}
+
+/**
+ * 0 = none
+ * 1 = zone
+ * 2 = redpoint
+ * 3 = top
+ */
+const resuls = [0, 1, 2, 3];
+function createResult(routeUuid: string, participantUuid: string): Result {
+  return {
+    uuid: faker.string.uuid(),
+    routeUuid,
+    participantUuid,
+    attempts: faker.number.int({ min: 1, max: 20 }),
+    result: resuls[Math.floor(Math.random() * resuls.length)],
+  };
+}
+
+function createAdmin(userUuid: string, competitionUuid: string): Admin {
+  return {
+    uuid: faker.string.uuid(),
+    userUuid,
+    competitionUuid,
+  }
+}
+
+async function seed() {
+  const users = [];
+  const competitions = [];
+  const participants = [];
+  const routes = [];
+  const result = [];
+  const admins = [];
+
+  for (let i = 0; i < 30; i++) {
+    users.push(createUser());
+  }
+
+  for (let i = 0; i < 3; i++) {
+    const competition = createCompetition();
+    const user = users[Math.floor(Math.random() * users.length)];
+    competitions.push(competition);
+    admins.push(createAdmin(user.uuid, competition.uuid));
+
+    for (let j = 0; j < 15; j++) {
+      const user = users[Math.floor(Math.random() * users.length)];
+      participants.push(createParticipant(user.uuid, competition.uuid));
+    }
+
+    for (let j = 0; j < 30; j++) {
+      const route = createRoute(competition.uuid);
+      routes.push(route);
+
+      const results = Math.floor(Math.random() * 10) + 5;
+
+      for (let j = 0; j < results; j++) {
+        const participant = participants[Math.floor(Math.random() * participants.length)];
+        result.push(createResult(route.uuid, participant.uuid));
+      }
+    }
+
+  }
+
+  await prisma.user.createMany({ data: users });
+  await prisma.competition.createMany({ data: competitions });
+  await prisma.route.createMany({ data: routes });
+  await prisma.participant.createMany({ data: participants });
+  await prisma.admin.createMany({ data: admins });
+  await prisma.result.createMany({ data: result });
+}
+
+try {
+  await seed();
+  await prisma.$disconnect();
+} catch (e) {
+  console.error(e);
+  await prisma.$disconnect();
+  process.exit(1);
+}
